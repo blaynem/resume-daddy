1ba37db33ec29a7c20c66402f0439e95c3a5ca40
Blayne Marjama <blayne.marjama@gmail.com>
2023-06-20 23:32:07 -0700

    okay how about now

--- apps/resume/app/onboarding/page.tsx
+++ apps/resume/app/onboarding/page.tsx
@@ -126,71 +126,70 @@ export default function Onboarding() {
       return;
     }
 
+    // User is now logged in!
+    router.push('/dashboard');
     return;
   };
 
   return (
     <div className="mx-auto max-w-7xl px-4 py-24 sm:px-6 sm:py-32 lg:px-8">
       <div className="mx-auto max-w-3xl p-4 bg-white border rounded-md border-violet-200">
-        <form autoComplete="on">
-          <Modal
-            initialFocusRef={initialRef}
-            isOpen={isOpen}
-            onClose={() => {
-              onClose();
-            }}
-          >
-            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-              <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                <img
-                  className="mx-auto h-10 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                  alt="Your Company"
-                />
-                <h2 className="mt-2 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                  {'Enter OTP code'}
-                </h2>
-                <p className="mt-2 text-center text-sm leading-5 text-gray-600 max-w">
-                  {'Enter the OTP code sent to your email'}
-                </p>
-              </div>
-
-              <div className="mt-2 sm:mx-auto sm:w-full sm:max-w-sm">
-                <form className="space-y-6" action="#" method="POST">
-                  <div className="mt-2">
-                    <input
-                      id="otp"
-                      name="otp"
-                      required
-                      value={otp}
-                      placeholder="XXXXXX"
-                      onChange={(e) => setOtp(e.target.value)}
-                      className="block w-full pl-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                  <button
-                    onClick={handleConfirm}
-                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  >
-                    Confirm
-                  </button>
-                </form>
-              </div>
-            </div>
-          </Modal>
-          <div className="space-y-12">
-            <div className="border-b border-gray-900/10 pb-12">
-              <h2 className="text-base font-semibold leading-7 text-gray-900">
-                Onboarding
+        <Modal
+          initialFocusRef={initialRef}
+          isOpen={isOpen}
+          onClose={() => {
+            onClose();
+          }}
+        >
+          <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+              <img
+                className="mx-auto h-10 w-auto"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                alt="Your Company"
+              />
+              <h2 className="mt-2 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                {'Enter OTP code'}
               </h2>
-              <p className="mt-1 text-sm leading-6 text-gray-600">
-                We know this is the least fun part of all. Trust us, just get
-                through a few questions and we will make your entire job
-                application process easier.
+              <p className="mt-2 text-center text-sm leading-5 text-gray-600 max-w">
+                {'Enter the OTP code sent to your email'}
               </p>
+            </div>
 
-              {/* TODO: Create a 'upload / paste' resume here */}
-              {/* <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="mt-2 sm:mx-auto sm:w-full sm:max-w-sm">
+              <div className="mt-2">
+                <input
+                  id="otp"
+                  name="otp"
+                  required
+                  value={otp}
+                  placeholder="XXXXXX"
+                  onChange={(e) => setOtp(e.target.value)}
+                  className="block w-full pl-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+              <button
+                onClick={handleConfirm}
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Confirm
+              </button>
+            </div>
+          </div>
+        </Modal>
+        <div className="space-y-12">
+          <div className="border-b border-gray-900/10 pb-12">
+            <h2 className="text-base font-semibold leading-7 text-gray-900">
+              Onboarding
+            </h2>
+            <p className="mt-1 text-sm leading-6 text-gray-600">
+              We know this is the least fun part of all. Trust us, just get
+              through a few questions and we will make your entire job
+              application process easier.
+            </p>
+
+            {/* TODO: Create a 'upload / paste' resume here */}
+            {/* <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                 <div className="col-span-full">
                   <label
                     htmlFor="photo"
@@ -212,132 +211,131 @@ export default function Onboarding() {
                   </div>
                 </div>
               </div> */}
-            </div>
+          </div>
 
-            <div className="border-b border-gray-900/10 pb-12">
-              <h2 className="text-base font-semibold leading-7 text-gray-900">
-                Personal Information
-              </h2>
-              <p className="mt-1 text-sm leading-6 text-gray-600">
-                {`Let's start with the basics.`}
-              </p>
+          <div className="border-b border-gray-900/10 pb-12">
+            <h2 className="text-base font-semibold leading-7 text-gray-900">
+              Personal Information
+            </h2>
+            <p className="mt-1 text-sm leading-6 text-gray-600">
+              {`Let's start with the basics.`}
+            </p>
 
-              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                <div className="sm:col-span-3">
-                  <label
-                    htmlFor="first-name"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    First name
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      required
-                      type="text"
-                      name="first-name"
-                      id="first-name"
-                      value={formState.firstName}
-                      onChange={(e) =>
-                        setFormState({
-                          ...formState,
-                          firstName: e.target.value,
-                        })
-                      }
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="first-name"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  First name
+                </label>
+                <div className="mt-2">
+                  <input
+                    required
+                    type="text"
+                    name="first-name"
+                    id="first-name"
+                    value={formState.firstName}
+                    onChange={(e) =>
+                      setFormState({
+                        ...formState,
+                        firstName: e.target.value,
+                      })
+                    }
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
                 </div>
+              </div>
 
-                <div className="sm:col-span-3">
-                  <label
-                    htmlFor="last-name"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Last name
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      required
-                      type="text"
-                      name="last-name"
-                      id="last-name"
-                      value={formState.lastName}
-                      onChange={(e) =>
-                        setFormState({
-                          ...formState,
-                          lastName: e.target.value,
-                        })
-                      }
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="last-name"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Last name
+                </label>
+                <div className="mt-2">
+                  <input
+                    required
+                    type="text"
+                    name="last-name"
+                    id="last-name"
+                    value={formState.lastName}
+                    onChange={(e) =>
+                      setFormState({
+                        ...formState,
+                        lastName: e.target.value,
+                      })
+                    }
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
                 </div>
+              </div>
 
-                <div className="sm:col-span-4">
-                  <label
-                    htmlFor="email"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Email address
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      required
-                      id="email"
-                      name="email"
-                      type="email"
-                      value={formState.email}
-                      onChange={(e) =>
-                        setFormState({
-                          ...formState,
-                          email: e.target.value,
-                        })
-                      }
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
+              <div className="sm:col-span-4">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Email address
+                </label>
+                <div className="mt-2">
+                  <input
+                    required
+                    id="email"
+                    name="email"
+                    type="email"
+                    value={formState.email}
+                    onChange={(e) =>
+                      setFormState({
+                        ...formState,
+                        email: e.target.value,
+                      })
+                    }
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
                 </div>
               </div>
             </div>
+          </div>
 
-            <div className="border-b border-gray-900/10 pb-12">
-              <h2 className="text-base font-semibold leading-7 text-gray-900">
-                Job Details
-              </h2>
-              <p className="mt-1 text-sm leading-6 text-gray-600">
-                {`Collecting some infromation on your past experience. Below we will
+          <div className="border-b border-gray-900/10 pb-12">
+            <h2 className="text-base font-semibold leading-7 text-gray-900">
+              Job Details
+            </h2>
+            <p className="mt-1 text-sm leading-6 text-gray-600">
+              {`Collecting some infromation on your past experience. Below we will
             dive into your role and experiences for each job you've had in
             the past. Don't worry about formatting, feel free to explain like you would to a friend.`}
-              </p>
-              <JobDetails
-                allJobDetails={allJobDetails}
-                onAddJob={() =>
-                  setAllJobDetails([...allJobDetails, templateJobDetails])
-                }
-                onRemoveJob={(index) => {
-                  const newJobDetails = [...allJobDetails];
-                  newJobDetails.splice(index, 1);
-                  setAllJobDetails(newJobDetails);
-                }}
-                onJobDetailsChange={(i, newAllDetails) => {
-                  const newJobDetails = [...allJobDetails];
-                  newJobDetails[i] = newAllDetails;
-                  setAllJobDetails(newJobDetails);
-                }}
-              />
-            </div>
+            </p>
+            <JobDetails
+              allJobDetails={allJobDetails}
+              onAddJob={() =>
+                setAllJobDetails([...allJobDetails, templateJobDetails])
+              }
+              onRemoveJob={(index) => {
+                const newJobDetails = [...allJobDetails];
+                newJobDetails.splice(index, 1);
+                setAllJobDetails(newJobDetails);
+              }}
+              onJobDetailsChange={(i, newAllDetails) => {
+                const newJobDetails = [...allJobDetails];
+                newJobDetails[i] = newAllDetails;
+                setAllJobDetails(newJobDetails);
+              }}
+            />
           </div>
+        </div>
 
-          <div className="mt-6 flex items-center justify-end gap-x-6">
-            <button
-              disabled={apiLoading}
-              onClick={handleNext}
-              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              {apiLoading ? <Spinner /> : 'Next'}
-            </button>
-          </div>
-        </form>
+        <div className="mt-6 flex items-center justify-end gap-x-6">
+          <button
+            disabled={apiLoading}
+            onClick={handleNext}
+            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            {apiLoading ? <Spinner /> : 'Next'}
+          </button>
+        </div>
       </div>
     </div>
   );
